32f6892cff12b4d5caa66b7f09ed053586d5ea77
sajeek0944 <sajeel6y4z@gmail.com>
2024-04-01 02:20:23 +0500

    todo list

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todolist = [];
+let condition = true;
+console.log(chalk.magentaBright.bgYellow.bold("\n\n\t\t Wellcome to SUK Todo list \n\n"));
+while (condition) {
+    let addtask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: chalk.blueBright("Enter your task :"),
+        },
+    ]);
+    todolist.push(addtask.todo);
+    console.log(chalk.redBright(`${addtask.todo} task added in Todo list`));
+    let addmoretask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: chalk.blueBright("Do you want to add more task"),
+            default: "false",
+        },
+    ]);
+    condition = addmoretask.addmore;
+}
+console.log(chalk.magenta("your Todo list"), todolist);
